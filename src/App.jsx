6a4950da5e12e58d408f7d2223818d5ac1f66570
dennis-tech <hedenniskibe@gmail.com>
2023-10-25 01:34:30 +0300

    added files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import "./app.scss"
+import Navbar from "./components/navbar/Navbar";
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    <section>
+      <Navbar />
+    </section>
+    <section id="Services">Parallax</section>
+    <section>Services</section>
+    <section id="Portfolio">Parallax</section>
+    <section>Porfolio1</section>
+    <section>Porfolio2</section>
+    <section>Porfolio3</section>
+    <section id="Contact">Contact</section>
+  </div>;
 };
 
 export default App;
