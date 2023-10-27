dade62b1c098b9ee062db285991a1393e086cd6b
dennis-tech <hedenniskibe@gmail.com>
2023-10-27 09:27:27 +0300

    added responsivity

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -63,7 +63,7 @@ const Hero = () => {
             Writer Content Creator Influencer
         </motion.div>
         <div className="imageContainer">
-            <img src="/hero.png" alt="" />
+            <img src="/hero.png" alt="Dennis Stanley Kibe" />
         </div>
     </div>
   )
